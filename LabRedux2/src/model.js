670f20fdf5910b62b899fa46d4016736ea6e14c1
alekshar <alekshar@Alekshar-UM>
2017-01-19 15:36:43 +0100

    LabRedux2 canvas implemented

---
+++ LabRedux2/src/model.js
@@ -0,0 +1,86 @@
+SensorFactory = {
+	create: function(data){
+		switch(data.type){
+		case "POSITIVE_NUMBER":
+			return new Temperature(data);
+		case "ON_OFF":
+			return new Switch(data);
+		case "OPEN_CLOSE":
+			return new Door(data);
+		case "PERCENT":
+			return new Gradient(data);
+		default:
+			throw "can't define sensor type of "+data.type;
+		}
+	}
+};
+
+function SensorManager(){
+	this.sensors = [];
+}
+
+SensorManager.prototype = {
+	manage:function(newSensor){
+		var id = newSensor.id;
+		for(var sensor of this.sensors){
+			if(sensor.id === id){
+				sensor.include(newSensor);
+				return sensor;
+			}
+		}
+		this.sensors.push(newSensor);
+		return sensor;
+	},
+	hasSensor:function(newSensor){
+		var id = newSensor.id;
+		for(var sensor of this.sensors){
+			if(sensor.id === id){
+				return true;
+			}
+		}
+		return false;
+	},
+	getSensor:function(id){
+		for(var sensor of this.sensors){
+			if(sensor.id === id){
+				return sensor;
+			}
+		}
+		return null;
+	}
+};
+
+
+function Sensor(data){
+	this.id = data.id;
+	this.values = [data.value];
+}
+
+Sensor.prototype.include = function(sensor){
+	this.values = this.values.concat(sensor.values);
+};
+
+Sensor.prototype.getLast = function(){
+	return this.values[this.values.length-1];
+};
+
+
+function Temperature(data){
+	Sensor.call(this, data);
+}
+Temperature.prototype = Object.create(Sensor.prototype);
+
+function Door(data){
+	Sensor.call(this, data);
+}
+Door.prototype = Object.create(Sensor.prototype);
+
+function Switch(data){
+	Sensor.call(this, data);
+}
+Switch.prototype = Object.create(Sensor.prototype);
+
+function Gradient(data){
+	Sensor.call(this, data);
+}
+Gradient.prototype = Object.create(Sensor.prototype);
